ac3950e229ebbd382935a6cf16ec34928729ee0c
dahmaular <dahmaular@gmail.com>
2024-03-21 15:05:57 +0100

    feature: hamburger for small screen

---
+++ html/script.js
@@ -0,0 +1,7 @@
+// console.log('connected')
+const mobileNav = document.querySelector('.mobile-nav-container')
+// console.log(mobileNav)
+const bars = document.querySelector('.bars-container')
+bars.addEventListener('click', () => {
+ 
+})
